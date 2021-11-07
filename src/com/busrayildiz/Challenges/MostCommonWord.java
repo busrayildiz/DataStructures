56146fa4ff763990bf1f75e1b4968d245378015e
busrayildiz <bbusrayildizz34@gmail.com>
2021-11-07 18:37:27 +0200

    MostCommonWord is solved on leetcode

---
+++ src/com/busrayildiz/Challenges/MostCommonWord.java
@@ -0,0 +1,62 @@
+package com.busrayildiz.Challenges;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+/**
+ * Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+ *
+ * The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+ *
+ * Example 1:
+ *
+ * Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+ * Output: "ball"
+ * Explanation:
+ * "hit" occurs 3 times, but it is a banned word.
+ * "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+ * Note that words in the paragraph are not case sensitive,
+ * that punctuation is ignored (even if adjacent to words, such as "ball,"),
+ * and that "hit" isn't the answer even though it occurs more because it is banned.
+ *
+ * **/
+
+class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+
+        Set<String> bannedWords = new HashSet();
+        for (String word : banned)
+            bannedWords.add(word);
+
+        String ans = "";
+        int maxCount = 0;
+        Map<String, Integer> wordCount = new HashMap();
+        StringBuilder wordBuffer = new StringBuilder();
+        char[] chars = paragraph.toCharArray();
+
+        for (int p = 0; p < chars.length; ++p) {
+            char currChar = chars[p];
+
+            if (Character.isLetter(currChar)) {
+                wordBuffer.append(Character.toLowerCase(currChar));
+                if (p != chars.length - 1)
+                    continue;
+            }
+
+            if (wordBuffer.length() > 0) {
+                String word = wordBuffer.toString();
+                if (!bannedWords.contains(word)) {
+                    int newCount = wordCount.getOrDefault(word, 0) + 1;
+                    wordCount.put(word, newCount);
+                    if (newCount > maxCount) {
+                        ans = word;
+                        maxCount = newCount;
+                    }
+                }
+                wordBuffer = new StringBuilder();
+            }
+        }
+        return ans;
+    }
+}
