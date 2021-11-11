ca1b3fb88dd4d1fbd0c1b60005104415411c57d0
busrayildiz <bbusrayildizz34@gmail.com>
2021-11-11 02:31:49 +0200

    Tail recursion implementation

--- src/com/busrayildiz/Recursion.java
+++ src/com/busrayildiz/Recursion.java
@@ -2,13 +2,24 @@ package com.busrayildiz;
 
 public class Recursion {
 
-    public static int recursiveFactorial(int num){
+    public static int recursiveFactorial(int num) {
 
-        if ( num == 0) {
+        if (num == 0) {
             return 1;
         }
 
         return num * recursiveFactorial(num - 1);
+    }
 
+
+    //space = O(1)
+    public static int tailRecursiveFactorial(int x , int totalSoFar) {
+
+        totalSoFar = 1;
+
+       if(x == 0)
+           return totalSoFar;
+       else
+           return tailRecursiveFactorial(x-1 , totalSoFar * x);
     }
 }
