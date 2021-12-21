abc9f3ea008abbeb3d357e71d02a383229671de3
busrayildiz <bbusrayildizz34@gmail.com>
2021-12-21 10:28:49 +0300

    Cracking the Coding Interview, Chapter 1

--- src/com/busrayildiz/CtCI/ArraysAndStrings.java
+++ src/com/busrayildiz/CtCI/ArraysAndStrings.java
@@ -1,6 +1,23 @@
 package com.busrayildiz.CtCI;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class ArraysAndStrings {
+
+    public static String sort(String str){
+        char[] s = str.toCharArray();
+        Arrays.sort(s);
+        return new String(s);
+    }
+
+    public static boolean checkPermutation(String s1, String s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        return sort(s1).equals(sort(s2));
+
+    }
 
     public static boolean isUnique(String str){
         if(str.length() > 128)
@@ -14,14 +31,13 @@ public class ArraysAndStrings {
             if(charArr[val]){
                 return false;
             }
-
             charArr[val]=true;
-
         }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println("isUnique(\"abcda\") = " + isUnique("abcd"));
+        //System.out.println("isUnique(\"abcda\") = " + isUnique("abcd"));
+        System.out.println(checkPermutation("abla", "bal"));
     }
 }
